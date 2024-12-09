45114c3b9a6b7c0dc419ca57ba632bd75f0bcc7e
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-09 21:28:21 +0100

    Added modules

---
+++ src/app/organizer/organizer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './organizer.component.html',
+  styleUrl: './organizer.component.css'
+})
+export class OrganizerComponent {
+
+}
---
+++ src/app/supporter/supporter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supporter',
+  standalone: true,
+  imports: [],
+  templateUrl: './supporter.component.html',
+  styleUrl: './supporter.component.css'
+})
+export class SupporterComponent {
+
+}
