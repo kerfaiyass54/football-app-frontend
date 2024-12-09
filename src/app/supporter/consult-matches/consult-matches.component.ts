11e15a627317546e1098d29eb822751cade6b657
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-09 21:46:02 +0100

    Added functions for supporter

---
+++ src/app/supporter/consult-matches/consult-matches.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-matches',
+  standalone: true,
+  imports: [],
+  templateUrl: './consult-matches.component.html',
+  styleUrl: './consult-matches.component.css'
+})
+export class ConsultMatchesComponent {
+
+}
---
+++ src/app/supporter/consult-tickets/consult-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './consult-tickets.component.html',
+  styleUrl: './consult-tickets.component.css'
+})
+export class ConsultTicketsComponent {
+
+}
