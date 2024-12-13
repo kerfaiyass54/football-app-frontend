411e7e355c8ca1f92a4d30cf32e1402f58ba2829
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:45:14 +0100

    added components for organizer for tournament teams

---
+++ src/app/organizer/tournament-editions/player-stats/player-stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-stats.component.html',
+  styleUrl: './player-stats.component.css'
+})
+export class PlayerStatsComponent {
+
+}
---
+++ src/app/organizer/tournament-editions/team-stats/team-stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-stats.component.html',
+  styleUrl: './team-stats.component.css'
+})
+export class TeamStatsComponent {
+
+}
