efb06cb2e30e1b10b19222b407314beeb5bba627
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:47:11 +0100

    added components for player

---
+++ src/app/player/consult-trainings/consult-trainings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-trainings',
+  standalone: true,
+  imports: [],
+  templateUrl: './consult-trainings.component.html',
+  styleUrl: './consult-trainings.component.css'
+})
+export class ConsultTrainingsComponent {
+
+}
