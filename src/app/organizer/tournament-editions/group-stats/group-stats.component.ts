08f0cf5e665019f981bc524a8f7058b02eb655fe
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:45:44 +0100

    added components for organizer for tournament groups

---
+++ src/app/organizer/tournament-editions/group-stats/group-stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-stats.component.html',
+  styleUrl: './group-stats.component.css'
+})
+export class GroupStatsComponent {
+
+}
