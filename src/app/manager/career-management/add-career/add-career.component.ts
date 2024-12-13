5069398c45f5f7b1b6b87514505bb0ec1c7b2fe3
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:28:17 +0100

    Added components for careers management

---
+++ src/app/manager/career-management/add-career/add-career.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-career',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-career.component.html',
+  styleUrl: './add-career.component.css'
+})
+export class AddCareerComponent {
+
+}
---
+++ src/app/manager/career-management/career-details/career-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-career-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './career-details.component.html',
+  styleUrl: './career-details.component.css'
+})
+export class CareerDetailsComponent {
+
+}
