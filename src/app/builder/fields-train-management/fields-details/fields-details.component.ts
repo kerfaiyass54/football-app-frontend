3192ab464f14164be107432db52acbdd0af5d169
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:22:59 +0100

    Added components for fields management

---
+++ src/app/builder/fields-train-management/fields-details/fields-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fields-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './fields-details.component.html',
+  styleUrl: './fields-details.component.css'
+})
+export class FieldsDetailsComponent {
+
+}
---
+++ src/app/builder/fields-train-management/make-upgrade/make-upgrade.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-make-upgrade',
+  standalone: true,
+  imports: [],
+  templateUrl: './make-upgrade.component.html',
+  styleUrl: './make-upgrade.component.css'
+})
+export class MakeUpgradeComponent {
+
+}
