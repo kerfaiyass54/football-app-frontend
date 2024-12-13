dc0dd73e20dcb3be2d83ed72fa5ed705d930ad03
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:55:36 +0100

    Added tickets for each match

---
+++ src/app/organizer/match-management/set-tickets/set-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-tickets.component.html',
+  styleUrl: './set-tickets.component.css'
+})
+export class SetTicketsComponent {
+
+}
