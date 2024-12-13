cb447911a164d972549d501aca96d365c6b4783a
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-13 21:54:40 +0100

    Added injuries for manager

---
+++ src/app/manager/see-injuries/see-injuries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-see-injuries',
+  standalone: true,
+  imports: [],
+  templateUrl: './see-injuries.component.html',
+  styleUrl: './see-injuries.component.css'
+})
+export class SeeInjuriesComponent {
+
+}
