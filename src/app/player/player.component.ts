125d99e428a4711e9358b3a0faf8971ec3d39599
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-23 20:18:19 +0100

    Added navbar for players

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {NavBarComponent} from "../Shared/nav-bar/nav-bar.component";
+import {RouterOutlet} from "@angular/router";
+import {NavbarElement} from "../Models/navbar-element";
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent,
+    RouterOutlet
+  ],
   templateUrl: './player.component.html',
   styleUrl: './player.component.css'
 })
 export class PlayerComponent {
+  navBarElements: NavbarElement[] = [{
+    name:'Matches view',
+    link:'/player/matches'
+  },
+    {
+      name:'Ratings view',
+      link:'/player/ratings'
+    },
+    {
+      name:'Trainings view',
+      link:'/player/trainings'
+    },
+    {
+      name:'Contracts management',
+      link:'/player/ratings'
+    },
+    {
+      name:'Stats view',
+      link:'/player/stats'
+    },
+    {
+      name:'Team information',
+      link:'/player/team'
+    }];
 
 }
