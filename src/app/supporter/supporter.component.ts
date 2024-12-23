e2d5369adfafb3e3da7df9f72dfcc1d97e58c0d8
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-23 20:20:00 +0100

    Added navbar for supporters

--- src/app/supporter/supporter.component.ts
+++ src/app/supporter/supporter.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {NavBarComponent} from "../Shared/nav-bar/nav-bar.component";
+import {RouterOutlet} from "@angular/router";
+import {NavbarElement} from "../Models/navbar-element";
 
 @Component({
   selector: 'app-supporter',
   standalone: true,
-  imports: [],
+    imports: [
+        NavBarComponent,
+        RouterOutlet
+    ],
   templateUrl: './supporter.component.html',
   styleUrl: './supporter.component.css'
 })
 export class SupporterComponent {
+  navBarElements: NavbarElement[]=[
+    {
+      name:'Location management',
+      link:'/supporter/location'
+    },
+    {
+      name:'Matches schedule',
+      link:'/supporter/matches'
+    },{
+      name:'Tickets buying',
+      link:'/supporter/tickets'
+    },
+  ];
 
 }
