ea72dc907b335e8aa3d6ec1124d4b3d4bed3fa99
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-23 20:23:02 +0100

    Added navbar for teams

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {NavBarComponent} from "../Shared/nav-bar/nav-bar.component";
+import {RouterOutlet} from "@angular/router";
+import {NavbarElement} from "../Models/navbar-element";
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent,
+    RouterOutlet
+  ],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
 export class TeamComponent {
+  navBarElements: NavbarElement[] = [
+    {
+      name:'Players view',
+      link:'/team/players'
+    },{
+      name:'Matches view',
+      link:'/team/matches'
+    },{
+      name:'Tournaments view',
+      link:'/team/tourns'
+    },{
+      name:'Transfers management',
+      link:'/team/transfer'
+    },{
+      name:'Manager view',
+      link:'/team/manager'
+    },
+  ];
 
 }
