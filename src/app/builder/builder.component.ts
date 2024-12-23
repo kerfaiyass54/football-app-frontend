6aa1492764dae8db5d5b4e67da3a3b7fee02ad3f
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-23 20:08:58 +0100

    Added navbar for managers and builders

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import {NavBarComponent} from "../Shared/nav-bar/nav-bar.component";
 import {NavbarElement} from "../Models/navbar-element";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-builder',
   standalone: true,
   imports: [
-    NavBarComponent
+    NavBarComponent,
+    RouterOutlet
   ],
   templateUrl: './builder.component.html',
   styleUrl: './builder.component.css'
 })
 export class BuilderComponent {
-  navBarElements: NavbarElement[]=[];
+  navBarElements: NavbarElement[]=[
+    {
+      name:'Fields management',
+      link:'/builder/fields'
+    },
+    {
+      name:'Stadiums management',
+      link:'/builder/stadiums'
+    }
+  ];
 
 }
--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import {NavBarComponent} from "../Shared/nav-bar/nav-bar.component";
+import {RouterOutlet} from "@angular/router";
+import {NavbarElement} from "../Models/navbar-element";
 
 @Component({
   selector: 'app-manager',
   standalone: true,
-  imports: [],
+    imports: [
+        NavBarComponent,
+        RouterOutlet
+    ],
   templateUrl: './manager.component.html',
   styleUrl: './manager.component.css'
 })
 export class ManagerComponent {
+  navBarElements: NavbarElement[]=[
+    {
+      name:'Careers information',
+      link:''
+    },
+    {
+      name:'Tournaments information',
+      link:''
+    },{
+      name:'Matches schedule',
+      link:''
+    },{
+      name:'Players management',
+      link:''
+    },{
+      name:'Injuries information',
+      link:''
+    },{
+      name:'Team information',
+      link:''
+    },
+  ];
 
 }
