42cc871abce63ce0ac52f080e8a1982eb52181f2
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-24 11:12:44 +0100

    Upgraded to v18

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app/app.component';
 import {KeycloakService} from "./app/Shared/services/keycloak.service";
 import {provideRouter} from "@angular/router";
 import {routes} from "./app/app.routes";
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 
 const keycloakService = new KeycloakService();
