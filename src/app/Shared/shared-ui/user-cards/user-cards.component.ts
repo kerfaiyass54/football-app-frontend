6bd8a713673a9bb995dc69b665e25ce6ee773246
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-17 22:58:34 +0100

    Added usable user interface

--- src/app/Shared/shared-ui/user-cards/user-cards.component.ts
+++ src/app/Shared/shared-ui/user-cards/user-cards.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-user-cards',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './user-cards.component.html',
   styleUrl: './user-cards.component.css',
 })
 export class UserCardsComponent {
+
+  @Input() cardsLinks: any[] = []
 
 }
