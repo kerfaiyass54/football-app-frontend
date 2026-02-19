175126130e596907f8a9e7315b5083a61f670eeb
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-19 15:36:04 +0100

    Added ui for searching to all the users

--- src/app/Shared/shared-ui/actions-ui/actions-ui.component.ts
+++ src/app/Shared/shared-ui/actions-ui/actions-ui.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {GenericTableComponent, TableColumn} from "../../../components/generic-table/generic-table.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-actions-ui',
-  imports: [],
+  imports: [
+    FormsModule,
+    GenericTableComponent
+  ],
   templateUrl: './actions-ui.component.html',
   styleUrl: './actions-ui.component.css',
 })
 export class ActionsUiComponent {
+  searchTerm = signal('');
+
+  // Selected rows
+  selectedUsers = signal<any[]>([]);
+
+  // Data
+  users: any[] = [
+    { id: 1, name: 'John Doe', email: 'john@mail.com', role: 'Admin' },
+    { id: 2, name: 'Jane Smith', email: 'jane@mail.com', role: 'any' },
+    { id: 3, name: 'Mike Brown', email: 'mike@mail.com', role: 'Manager' }
+  ];
+
+  columns: TableColumn<any>[] = [
+    { key: 'name', label: 'Name', sortable: true },
+    { key: 'email', label: 'Email' },
+    { key: 'role', label: 'Role', sortable: true }
+  ];
+
+  // Filtered data (external search)
+  filteredUsers = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+    if (!term) return this.users;
+
+    return this.users.filter(u =>
+      Object.values(u).some(val =>
+        String(val).toLowerCase().includes(term)
+      )
+    );
+  });
+
+
+  onSelectionChange(users: any[]) {
+    this.selectedUsers.set(users);
+  }
+
+  seeDetails() {
+    const user = this.selectedUsers()[0];
+    if (!user) return;
+
+    console.log('Selected user:', user);
+  }
 
 }
--- src/app/admin/user-management/builders-management/builders-actions/builders-actions.component.ts
+++ src/app/admin/user-management/builders-management/builders-actions/builders-actions.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {PreviousButtonComponent} from "../../../../components/buttons/previous-button/previous-button.component";
+import {ActionsUiComponent} from "../../../../Shared/shared-ui/actions-ui/actions-ui.component";
 
 @Component({
   selector: 'app-builders-actions',
-  imports: [],
+  imports: [
+    PreviousButtonComponent,
+    ActionsUiComponent
+  ],
   templateUrl: './builders-actions.component.html',
   styleUrl: './builders-actions.component.css',
 })
 export class BuildersActionsComponent {
+
 
 }
