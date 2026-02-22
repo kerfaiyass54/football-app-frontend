7fa7ad2d01b9b35ef76075d7dbaa92328e5f0649
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-22 15:39:56 +0100

    Added bootstrap

--- src/app/admin/user-management/builders-management/builder-details/builder-details.component.ts
+++ src/app/admin/user-management/builders-management/builder-details/builder-details.component.ts
@@ -1,22 +1,30 @@
-import {Component, OnInit, signal} from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BuilderService } from '../../../services/builder.service';
-import {PreviousButtonComponent} from "../../../../components/buttons/previous-button/previous-button.component";
-import {COUNTRIES} from "../../../../Shared/constants/countries";
-import {LoaderComponent} from "../../../../components/loader/loader.component";
+import { PreviousButtonComponent } from "../../../../components/buttons/previous-button/previous-button.component";
+import { LoaderComponent } from "../../../../components/loader/loader.component";
+import { ModalComponent } from "../../../../components/modal/modal.component";
+import { COUNTRIES } from "../../../../Shared/constants/countries";
 
 @Component({
   selector: 'app-builder-details',
   standalone: true,
-  imports: [CommonModule, PreviousButtonComponent, LoaderComponent],
+  imports: [
+    CommonModule,
+    PreviousButtonComponent,
+    LoaderComponent,
+    ModalComponent
+  ],
   templateUrl: './builder-details.component.html',
   styleUrl: './builder-details.component.css'
 })
 export class BuilderDetailsComponent implements OnInit {
 
-  builder: any;
+  builder: any = null;
+
   loading = signal(true);
+  showDeleteModal = signal(false);
 
   constructor(
     private route: ActivatedRoute,
@@ -27,13 +35,17 @@ export class BuilderDetailsComponent implements OnInit {
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
 
-    if (id) {
-      this.builderService.getBuilderById(id).subscribe(res => {
-        this.builder = res;
-        setTimeout(() => { this.loading.set(false); }, 1500);
+    if (!id) return;
 
-      });
-    }
+    this.builderService.getBuilderById(+id).subscribe({
+      next: (res) => {
+        this.builder = res;
+        this.loading.set(false);
+      },
+      error: () => {
+        this.loading.set(false);
+      }
+    });
   }
 
   updatePrice() {
@@ -41,7 +53,22 @@ export class BuilderDetailsComponent implements OnInit {
   }
 
   deleteBuilder() {
-    console.log('Delete clicked');
+    this.showDeleteModal.set(false);
+
+    // Example real delete
+    /*
+    this.loading.set(true);
+
+    this.builderService.deleteBuilder(this.builder.id).subscribe({
+      next: () => {
+        this.loading.set(false);
+        this.router.navigate(['/admin/builders']);
+      },
+      error: () => {
+        this.loading.set(false);
+      }
+    });
+    */
   }
 
   get countryCode(): string {
