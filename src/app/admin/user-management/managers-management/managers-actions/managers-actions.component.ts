aa3e61008b5136bc6cdb77b57a1202c4ce297f26
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-23 23:37:15 +0100

    Added stats for the manager and details

--- src/app/admin/user-management/managers-management/managers-actions/managers-actions.component.ts
+++ src/app/admin/user-management/managers-management/managers-actions/managers-actions.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, OnInit } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import {GenericTableComponent} from "../../../../components/generic-table/generic-table.component";
+import {ManagerService} from "../../../services/manager.service";
+import {MANAGER_TABLE_COLUMNS} from "../../../../core/constants/manager.constants";
+import {PreviousButtonComponent} from "../../../../components/buttons/previous-button/previous-button.component";
 
 @Component({
   selector: 'app-managers-actions',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    GenericTableComponent,
+    PreviousButtonComponent
+  ],
   templateUrl: './managers-actions.component.html',
   styleUrl: './managers-actions.component.css',
 })
-export class ManagersActionsComponent {
+export class ManagersActionsComponent implements OnInit {
 
+  constructor(private managerService: ManagerService, private router: Router) {}
+
+  searchTerm = signal('');
+  selectedManagers = signal<any[]>([]);
+  managers = signal<any[]>([]);
+
+  // ✅ MANAGER COLUMNS
+  columns = MANAGER_TABLE_COLUMNS;
+
+  ngOnInit(): void {
+    this.loadManagers();
+  }
+
+  loadManagers() {
+    this.managerService.getManagers(0, 20).subscribe({
+      next: (res: any) => {
+        this.managers.set(res.content);
+      },
+      error: (err: any) => console.error(err)
+    });
+  }
+
+  filteredManagers = computed(() => {
+    const term = this.searchTerm().toLowerCase();
+    if (!term) return this.managers();
+
+    return this.managers().filter(m =>
+      Object.values(m).some(val =>
+        String(val).toLowerCase().includes(term)
+      )
+    );
+  });
+
+  onSelectionChange(data: any[]) {
+    this.selectedManagers.set(data);
+  }
+
+  seeDetails() {
+    const manager = this.selectedManagers()[0];
+    if (!manager) return;
+
+    this.router.navigate(['/admin/manager-details', manager.id]);
+  }
 }
