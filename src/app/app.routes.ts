be7d2cba487ac14acc2a7c753be9662e56e081de
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-24 16:23:51 +0100

    Added team services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,8 +8,6 @@ import {SupporterComponent} from "./supporter/supporter.component";
 import {TeamComponent} from "./team/team.component";
 import {UserManagementComponent} from "./admin/user-management/user-management.component";
 import {LocationManagementComponent} from "./admin/location-management/location-management.component";
-import {AddLocationComponent} from "./admin/location-management/add-location/add-location.component";
-import {DetailsLocationComponent} from "./admin/location-management/details-location/details-location.component";
 import {FieldsTrainManagementComponent} from "./builder/fields-train-management/fields-train-management.component";
 import {FieldsDetailsComponent} from "./builder/fields-train-management/fields-details/fields-details.component";
 import {AddFieldComponent} from "./builder/fields-train-management/add-field/add-field.component";
@@ -141,12 +139,6 @@ export const routes: Routes = [
         },{
           path:'locations',
           component:LocationManagementComponent
-        },{
-          path:'add-location',
-          component:AddLocationComponent
-        },{
-          path:'details-location/:id',
-          component:DetailsLocationComponent
         },{
           path:'details-builders/:id',
           component:BuilderDetailsComponent
